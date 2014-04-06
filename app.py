c1400f5253d71c8456437234d65923e304620889
Jesse Huang <jh3141@gmail.com>
2014-04-06 02:01:15 +0000

    file uploading and editing works

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import os
+import requests
+import urllib2
 
 app = Flask(__name__)
 
@@ -20,7 +22,30 @@ def aviary():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-  print(request.form['url'])
+  url = request.form['url']
+  print url
+  file_name = url.split('/')[-1]
+  u = urllib2.urlopen(url)
+  f = open(file_name, 'wb')
+  meta = u.info()
+  file_size = int(meta.getheaders("Content-Length")[0])
+  print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+  file_size_dl = 0
+  block_sz = 8192
+  while True:
+      buffer = u.read(block_sz)
+      if not buffer:
+          break
+
+      file_size_dl += len(buffer)
+      f.write(buffer)
+      status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+      status = status + chr(8)*(len(status)+1)
+      print status,
+
+  f.close()
+
   return "200"
 
 if __name__ == '__main__':
